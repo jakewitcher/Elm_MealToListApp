3f8f3babc723c070562c66ef7d8469035c4777e3
Jake Witcher <jjwitcher09@gmail.com>
2018-10-27 15:30:19 -0400

    Remove ejs as view engine

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>
-    res.render('index.ejs'));
+    res.render('index'));
 
 app.listen(port, () =>
     console.log(`The server has started at port ${port}`));
