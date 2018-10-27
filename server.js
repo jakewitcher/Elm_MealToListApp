70f93a247733060c6adaf6cc69c2a04ea511b925
Jake Witcher <jjwitcher09@gmail.com>
2018-10-27 15:02:12 -0400

    Add ejs for view render

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>
-    res.sendFile('index.html'));
+    res.render('index'));
 
 app.listen(port, () =>
     console.log(`The server has started at port ${port}`));
