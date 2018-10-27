f143d57000728b140f4cdd5b3f08e7a2d1d5131e
Jake Witcher <jjwitcher09@gmail.com>
2018-10-27 15:14:16 -0400

    Change port env variable to PORT

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>
-    res.render('index'));
+    res.render('index.ejs'));
 
 app.listen(port, () =>
     console.log(`The server has started at port ${port}`));
